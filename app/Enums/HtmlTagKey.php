f96337719b5dd545182b5a4a09673ee8c8b4e7ef
j2cey <jud10parfait@gmail.com>
2023-04-17 14:06:22 +0000

    Enum Attribute extend

--- app/Enums/HtmlTagKey.php
+++ app/Enums/HtmlTagKey.php
@@ -2,12 +2,28 @@
 
 namespace App\Enums;
 
+use App\Traits\Enum\EnumTrait;
+use App\Enums\Attributes\Description;
+
 enum HtmlTagKey: string
 {
+    use EnumTrait;
+
+    #[Description('Tableau')]
     case TABLE = 'table';
+
+    #[Description('Ligne de Tableau')]
     case TABLE_ROW = 'table_row';
+
+    #[Description('Colonne de Tableau')]
     case TABLE_COL = 'table_col';
+
+    #[Description('Gras')]
     case BOLD = 'bold';
+
+    #[Description('Italique')]
     case ITALIC = 'italic';
+
+    #[Description('Souligne')]
     case UNDERLINE = 'underline';
 }
--- app/Traits/Enum/EnumTrait.php
+++ app/Traits/Enum/EnumTrait.php
@@ -2,17 +2,53 @@
 
 namespace App\Traits\Enum;
 
+use Illuminate\Support\Str;
+use ReflectionClassConstant;
+use App\Enums\Attributes\Description;
+
 trait EnumTrait
 {
     public static function toAssociativeArray(): array
     {
         foreach(self::cases() as $case) {
-            $array[] = ['name' => $case->name, 'value' => $case->value];
+            $array[] = ['name' => self::getDescription($case), 'value' => $case->value];
+            //$array[] = ['name' => $case->name, 'value' => $case->value];
         }
         return $array;
     }
 
     public function toArray() {
-        return ['name' => $this->name, 'value' => $this->value];
+        return ['name' => self::getDescription($this), 'value' => $this->value];
+        //return ['name' => $this->name, 'value' => $this->value];
+    }
+
+    /**
+     * @param self $enum
+     */
+    private static function getDescription(self $enum): string
+    {
+        $ref = new ReflectionClassConstant(self::class, $enum->name);
+        $classAttributes = $ref->getAttributes(Description::class);
+
+        if (count($classAttributes) === 0) {
+            return Str::headline($enum->value);
+        }
+
+        return $classAttributes[0]->newInstance()->description;
+    }
+
+    /**
+     * @return array<string,string>
+     */
+    public static function asSelectArray(): array
+    {
+        /** @var array<string,string> $values */
+        return collect(self::cases())
+            ->map(function ($enum) {
+                return [
+                    'name' => self::getDescription($enum),
+                    'value' => $enum->value,
+                ];
+            })->toArray();
     }
 }
